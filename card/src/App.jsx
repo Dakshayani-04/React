070a91230e2293378e31c99976062b7b4ff5f28f
Dakshayani-04 <23a91a1211@aec.edu.in>
2025-10-07 09:33:54 +0530

    third commit

--- card/src/App.jsx
+++ card/src/App.jsx
@@ -5,15 +5,26 @@ import './App.css'
 // import Card from './CardMain'
 import Function from './FunctionComponent'
 import myimage from './assets/react.svg';
-import Component1 from './Component1'
-import Component2 from './Component2'
+import Home from './Home'
+import Services from './Services'
+import Contact from './Contact'
+import PageNotFound from './PageNotFound'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+
 
 function App() {
 
 
   return (
     <>
-     
+     <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/services" element={<Services/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="*" element ={<PageNotFound/>}/>
+     </Routes>
+     </BrowserRouter>
     </> 
   )
 }
---
+++ card/src/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Home = ()=>{
+    return(
+        <>
+        <h1>Home Page</h1>
+        <p>This is a home page which is also gives complete details of page in this particular page</p>
+        </>
+    )
+}
+export default Home;
